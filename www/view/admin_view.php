d2f3a7c4ee119febaec2f6cf6b8ab696f5dba25f
ozin0206 <ozin0206@gmail.com>
2020-07-22 21:05:29 +0900

    #2

--- www/view/admin_view.php
+++ www/view/admin_view.php
@@ -47,7 +47,6 @@
       <input type="submit" value="商品追加" class="btn btn-primary">
     </form>
 
-
     <?php if(count($items) > 0){ ?>
       <table class="table table-bordered text-center">
         <thead class="thead-light">
@@ -61,19 +60,19 @@
         </thead>
         <tbody>
           <?php foreach($items as $item){ ?>
-          <tr class="<?php print(is_open($item) ? '' : 'close_item'); ?>">
+          <tr class="<?php print (is_open($item) ? '' : 'close_item'); ?>">
             <td><img src="<?php print(IMAGE_PATH . $item['image']);?>" class="item_image"></td>
-            <td><?php print($item['name']); ?></td>
-            <td><?php print(number_format($item['price'])); ?>円</td>
+            <td><?php print h($item['name']); ?></td>
+            <td><?php print h(number_format($item['price'])); ?>円</td>
             <td>
               <form method="post" action="admin_change_stock.php">
                 <div class="form-group">
                   <!-- sqlインジェクション確認のためあえてtext -->
-                  <input  type="text" name="stock" value="<?php print($item['stock']); ?>">
+                  <input  type="text" name="stock" value="<?php print h($item['stock']); ?>">
                   個
                 </div>
                 <input type="submit" value="変更" class="btn btn-secondary">
-                <input type="hidden" name="item_id" value="<?php print($item['item_id']); ?>">
+                <input type="hidden" name="item_id" value="<?php print h($item['item_id']); ?>">
               </form>
             </td>
             <td>
--- www/view/cart_view.php
+++ www/view/cart_view.php
@@ -32,13 +32,13 @@
             <td><?php print(number_format($cart['price'])); ?>円</td>
             <td>
               <form method="post" action="cart_change_amount.php">
-                <input type="number" name="amount" value="<?php print($cart['amount']); ?>">
+                <input type="number" name="amount" value="<?php print h($cart['amount']); ?>">
                 個
                 <input type="submit" value="変更" class="btn btn-secondary">
-                <input type="hidden" name="cart_id" value="<?php print($cart['cart_id']); ?>">
+                <input type="hidden" name="cart_id" value="<?php print h($cart['cart_id']); ?>">
               </form>
             </td>
-            <td><?php print(number_format($cart['price'] * $cart['amount'])); ?>円</td>
+            <td><?php print h(number_format($cart['price'] * $cart['amount'])); ?>円</td>
             <td>
 
               <form method="post" action="cart_delete_cart.php">
